ba1f019c269824b07d55c0d8eca1b146020ad9c0
Valdeir Psr <valdeir.naval@gmail.com>
2020-08-19 13:52:41 -0300

    fix: renomeia arquivo main.js -> index.js

---
+++ check-issues/index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+core.setOutput('Número do issue', github.context.issue.number)
+core.setOutput('Repo', github.context.issue.repo)
+core.setOutput('number', '123456789')
+
+module.exports = (':/')
+
+return 'Poxa...'

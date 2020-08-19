9976022d60a230aa2b325fb9ae30cb7f6d43b3ad
Valdeir Santana <valdeirpsr@hotmail.com>
2020-08-19 17:02:08 -0300

    Update index.js

--- check-issues/index.js
+++ check-issues/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 core.setOutput('Número do issue', github.context.issue.number)
-core.setOutput('Repo', github.context.issue.repo)
+core.setOutput('Repo', ':: Nome do Repositório ::')
 core.setOutput('number', '123456789')
 
 module.exports = (':/')

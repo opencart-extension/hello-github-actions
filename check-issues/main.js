7ae4811915e6aaab69520a1539b7fba1631615bb
Valdeir Psr <valdeir.naval@gmail.com>
2020-08-19 13:50:42 -0300

    refactor: renomeia arquivos

--- check-issues/main.js
+++
@@ -1,6 +0,0 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-
-core.setOutput('Número do issue', github.context.issue.number)
-core.setOutput('Repo', github.context.issue.repo)
-core.setOutput('number', '123456789')

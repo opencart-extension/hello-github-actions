eef1dc34f17d65763578c46cf081a47d6864b3a3
Valdeir Psr <valdeir.naval@gmail.com>
2020-09-13 09:48:34 -0300

    A

--- .github/actions/doc-deploy/entrypoint.sh
+++ .github/actions/doc-deploy/entrypoint.sh
@@ -7,3 +7,13 @@ echo '' # see https://github.com/actions/toolkit/issues/168
 npm install && npm run build
 
 DEPLOY_REPO="https://${ACCESS_TOKEN}@github.com/${REPOSITORY_NAME}.git"
+
+git checkout gh-pages
+
+git checkout documentation -- src/.vuepress/dist
+
+git add -A
+
+git commit -m "Auto deploy"
+
+git push -f $DEPLOY_REPO gh-pages
